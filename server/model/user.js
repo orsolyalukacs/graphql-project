dbba93ad1b26c790097f30b785760e75f3cf1fad
ai4i <toth.pala@gmail.com>
2020-04-13 15:46:31 -0500

    Add mongoose models

---
+++ server/model/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const MSchema = mongoose.Schema;
+
+/*  create the model
+    constructing our schema to allow us create data in mongoDb
+ */
+
+const userSchema = new MSchema({
+    name: String,
+    age: Number,
+    profession: String
+})
+
+module.exports = mongoose.model('User', userSchema);
